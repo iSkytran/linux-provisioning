a38c8d31a28480cebdd51b27f6643c4a3339fbe8
iSkytran <rizcielbleu@gmail.com>
2020-10-11 00:55:28 -0400

    Initial arch scripts

---
+++ arch_install.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+# Checks for internet
+while ! ping -q -c 1 -W 1 8.8.8.8 >> /dev/null
+do
+    printf "Waiting for internet..."
+    sleep 1
+done
+
+# Update system clock
+timedatectl set-ntp true
+
+# Prompt user for partitions
+printf ("Make 3 disk partitions, a 512M EFI System on /dev/sdX1, "
+   "a 2G linux swap on /dev/sdX2, "
+   "and a Linux filesystem using the rest of the disk on /dev/sdX3.\n"
+   "Make note of the drive name (e.g. /dev/sda)"
+   "Press enter to continue...")
+read
+cfdisk
+read -p "Ender the drive name (default: /dev/sda): " drive_name
+drive_name=${drive_name:-"/dev/sda"}
+mkfs.fat -F32 ${drive_name}1
+mkswap ${drive_name}2
+mkfs.ext4 ${drive_name}3
+
+# Set mirror
+pacman -Syy
+pacman -S reflector
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+# Install the system
+mount /dev/sda3 /mnt
+pacstrap /mnt base linux linux-firmware base-devel man-db man-pages texinfo networkmanager ufw curl git vim zsh tmux openssh python jdk-openjdk docker docker-compose
+
+# Generate fstab and enter mounted disk as root
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+
+# Set time zone
+read -p "Enter the time zone (default: America/New_York): " time_zone
+time_zone=${time_zone:-"America/New_York"}
+ln -sf /usr/share/zoneinfo/${time_zone} /etc/localtime
+hwclock --systohc
+
+# Setup locale
+sed '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+locale-gen
+printf "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Network configuration
+read -p "Enter the hostname of the computer: " host_name
+touch /etc/hosts
+printf ("127.0.0.1\tlocalhost\n"
+    "::1\tlocalhost\n127.0.1.1\t"
+    "${host_name}.localdomain\t${host_name}") >> /etc/hosts
+printf $host_name >> /etc/hostname
+systemctl enable NetworkManager.service
+
+# Firewall setup
+systemctl enable --now ufw.service
+ufw default deny
+ufw allow from 192.168.0.0/24
+ufw limit ssh
+ufw enable
+
+# Docker setup
+systemctl enable --now docker.service
+
+#Set root password
+printf "Set root password..."
+passwd
+
+# Install bootloader
+microcode_type=""
+while (microcode_type == "")
+do
+    printf "Choose microcode updates (intel, amd, none): "
+    read microcode_type
+    case $microcode_type in
+    "intel")
+        pacman -S intel-ucode
+        ;;
+    "amd")
+        pacman -S amd-ucode
+        ;;
+    "none")
+        break
+        ;;
+    *)
+        printf "Invalid selection."
+        microcode_type=""
+        ;;
+    esac
+done
+pacman -S grub efibootmgr
+mkdir /boot/efi
+mount ${drive_name}1 /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Add default user
+read -p "Adding default user, enter username: " user_name
+useradd -m -G wheel -s /usr/bin/zsh $user_name
+printf "Set user password..."
+passwd $user_name
+
+# Install yay package manager
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -r yay
+sed '/Color/s/^#//g' /etc/pacman.conf
+
+# Upgrade system to make sure everything is up to date
+yay -Syu
+
+# Install graphics driver
+graphics_type=""
+while (graphics_type == "")
+do
+    printf "Choose graphics driver (intel, amd, nvidia): "
+    read graphics_type
+    case $graphics_type in
+    "intel")
+        yay -S xf86-video-intel
+        ;;
+    "amd")
+        yay -S xf86-video-amdgpu
+        ;;
+    "nvidia")
+        yay -S nvidia
+        ;;
+    *)
+        printf "Invalid selection."
+        graphics_type=""
+        ;;
+    esac
+done
+
+# Install user software
+yay -S zsh keepassxc visual-studio-code-bin google-chrome dropbox
+
+# Install i3 and related software
+yay -S xorg-server xorg-xinit i3-wm dmenu i3lock i3status picom hsetroot rxvt-unicode rofi vlc
+printf "exec i3" >> /etc/X11/xinit/xinitrc
+
+# Download post install script
+curl -L https://raw.githubusercontent.com/iSkytran/linux-install-scripts -o /home/${user_name}/arch_post_install.sh
+chmod a+x /home/${user_name}/arch_post_install.sh
+
+# Switch to default user
+printf "Switching to default user..."
+su -c /home/${user_name}/arch_post_install.sh $user_name
---
+++ arch_post_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Configure zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+curl -L https://raw.githubusercontent.com/iSkytran/dotfiles/master/.p10k.zsh -o .p10k.zsh
+curl -L https://raw.githubusercontent.com/iSkytran/dotfiles/master/.zshrc -o .zshrc
+
+# Font and background download
+curl -L https://cdn.spacetelescope.org/archives/images/wallpaper4/heic1509a.jpg -o ~/.wallpaper.jpg
+curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete%20Mono.ttf -o ~/.local/share/fonts/"JetBrains Mono Regular Nerd Font Complete Mono.ttf" --create-dirs
+fc-cache
+
+# Download i3 dotfiles
+curl -L https://raw.githubusercontent.com/iSkytran/dotfiles/master/.config/i3/config -o ~/.config/i3/config --create-dirs
+curl -L https://
+
+# Generate ssh files
+printf "Generating ssh files..."
+ssh-keygen -t rsa -b 4096
+
+# Final message
+printf ("Restart the computer to finish. Run nmtui to connect to internet. "
+    "Sign into Google Chrome, VS Code, and Dropbox to sync preferences. "
+    "Copy KeePass key file to computer and copy contents of public ssh key to GitHub."
+    "Delete /root/arch_install.sh and ~/arch_post_install.")
