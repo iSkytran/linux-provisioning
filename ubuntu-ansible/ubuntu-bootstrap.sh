5eeefd819300755665d60a038571e0f831b33617
Ethan Tran <rizcielbleu@gmail.com>
2020-11-11 14:55:27 -0500

    Add python3-venv package and switch to ansible-base

--- ubuntu-ansible/ubuntu-bootstrap.sh
+++ ubuntu-ansible/ubuntu-bootstrap.sh
@@ -5,12 +5,12 @@ cd ~
 
 # Install python 3
 sudo apt update
-sudo apt install git python3 python3-pip -y
+sudo apt install git python3 python3-venv python3-pip -y
 
 # Install pip packages
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
-pipx install ansible
+pipx install ansible-base
 
 # Clone provisioning repository
 git clone https://github.com/iSkytran/linux-provisioning.git
