178de27d6497af1e358cfe377bb1f2cb2e32da97
iSkytran <rizcielbleu@gmail.com>
2020-11-10 20:44:55 -0500

    Add yes switches and fix oneliner boostrap command

--- ubuntu-ansible/ubuntu-ansible.md
+++ ubuntu-ansible/ubuntu-ansible.md
@@ -3,5 +3,5 @@
 Use the bootstrap script to use Ansible to setup the development environment.
 
 ```sh
-sh -c "$(curl -sSL https://raw.githubusercontent.com/iSkytran/linux-provisioning/main/ubuntu-ansible/ubuntu-bootstrap.sh)"
+curl -sSL "https://raw.githubusercontent.com/iSkytran/linux-provisioning/main/ubuntu-ansible/ubuntu-bootstrap.sh" | bash -s
 ```
--- ubuntu-ansible/ubuntu-bootstrap.sh
+++ ubuntu-ansible/ubuntu-bootstrap.sh
@@ -2,15 +2,12 @@ $!/bin/bash
 
 # Install Ansible and its dependencies
 sudo apt update
-sudo apt install software-properties-common git python3 python3-pip
+sudo apt install software-properties-common git python3 python3-pip -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 
 # Clone provisioning repository
 git clone git@github.com:iSkytran/linux-provisioning.git
 
 # Run playbook
 ansible-playbook ./linux-provisioning/ubuntu-ansible/ubuntu-playbook.yml
-
-# Remove this bootstrap script
-rm ubuntu-bootstrap.sh
