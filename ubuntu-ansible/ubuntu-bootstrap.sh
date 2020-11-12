2a80bd2928bba0a61e6ffc4f5ba790c8bf0dc6a3
Ethan Tran <rizcielbleu@gmail.com>
2020-11-11 19:32:08 -0500

    Fix pipx commands

--- ubuntu-ansible/ubuntu-bootstrap.sh
+++ ubuntu-ansible/ubuntu-bootstrap.sh
@@ -10,7 +10,7 @@ sudo apt install git python3 python3-venv python3-pip -y
 # Install pip packages
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
-pipx install ansible-base
+python3 -m pipx install ansible-base
 
 # Clone provisioning repository
 git clone https://github.com/iSkytran/linux-provisioning.git
